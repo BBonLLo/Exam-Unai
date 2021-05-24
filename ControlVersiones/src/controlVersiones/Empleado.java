4f6dd918fa2909a8e1d00df2ef4b80c3c3e2113a
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2021-05-24 09:29:06 +0200

    Segundo commit el primero se me ha borrado

--- ControlVersiones/src/controlVersiones/Empleado.java
+++ ControlVersiones/src/controlVersiones/Empleado.java
@@ -1,9 +1,27 @@
 package controlVersiones;
-
+/**
+ * 
+ * @author unai
+ * @version Version 1, 2021
+ * @since 24/05/2021
+ *
+ */
 public class Empleado {
+	/**
+	 * nombre Nombre del empleado
+	 * apellidos Apellidos del empleado
+	 * salario Salario del empleado
+	 */
 	private String nombre;
 	private String apellidos;
 	private double salario;
+	/**
+	 * 
+	 * @param nombre Contiene el nombre del empleado
+	 * @param apellido Contiene el apellido del empleado
+	 * @param salario Contiene el salario del empleado
+	 * 
+	 */
 
 	public Empleado(String nombre, String apellido, double salario) {
 		this.nombre = nombre;
@@ -38,5 +56,9 @@ public class Empleado {
 	public void subirsalario(double subida) {
 		salario = salario + subida;
 	}
+	/**
+	 * 
+	 * @param subida Añade al salario un extra llamado subida
+	 */
 
 }
